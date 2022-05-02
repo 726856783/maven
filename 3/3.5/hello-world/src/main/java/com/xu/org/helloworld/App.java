6345a6b5cbffae2654cae242ac1898beb62c580b
xulian <726856783@qq.com>
2022-05-02 16:51:46 +0800

    3.5

---
+++ 3/3.5/hello-world/src/main/java/com/xu/org/helloworld/App.java
@@ -0,0 +1,13 @@
+package com.xu.org.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
